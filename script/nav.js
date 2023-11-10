fe7eb31780bbcc02a585476b8d26ac46decf6ae4
Miniture <brendenmaxwell@yahoo.com>
2023-11-09 20:09:48 -0500

    Add files via upload
    added pages for tutorial, bad example, comprehension check. Changed nav to js.

---
+++ script/nav.js
@@ -0,0 +1,33 @@
+nav.innerHTML = `
+
+		<img class="navbar-brand ms-2" src="img/Flag_of_Canada.svg" alt="Temp Logo - Flag of Canada" width="" height="40">
+		<a class="nav-link pe-4" href="">Home</a>
+
+		<!-- collapsed hamburger menu button -->
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+  			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+			<ul class="navbar-nav mr-auto mt-2 mt-md-0">
+				<li class="nav-item dropdown">
+		          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+		            Table of Contents
+		          </a>
+		          <ul class="dropdown-menu">
+		            <li><a class="dropdown-item" href="tutorial.html">Tutorial</a></li>
+		            <li><a class="dropdown-item" href="bad.html">Tutorial but bad?</a></li>
+		            <li><a class="dropdown-item" href="#">A third thing</a></li>
+		          </ul>
+		        </li>
+
+				<li class="nav-item px-4">
+					<a class="nav-link" href="">Get Started</a>
+				</li>
+				<li class="nav-item px-4">
+					<a class="nav-link" href="comprehension.html">Comprehension Check</a>
+				</li>
+			</ul>
+		</div>
+	
+`
